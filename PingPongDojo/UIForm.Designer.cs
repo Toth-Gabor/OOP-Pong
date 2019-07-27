23283f4ea8eebc86ff42b7bdd064c163be6a853f
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-07-27 13:59:33 +0200

    Added paddle and ball

--- PingPongDojo/UIForm.Designer.cs
+++ PingPongDojo/UIForm.Designer.cs
@@ -28,21 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.playGround = new System.Windows.Forms.FlowLayoutPanel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.paddle = new System.Windows.Forms.PictureBox();
+            this.playGround.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle)).BeginInit();
             this.SuspendLayout();
             // 
-            // ui
+            // playGround
+            // 
+            this.playGround.Controls.Add(this.ball);
+            this.playGround.Controls.Add(this.paddle);
+            this.playGround.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playGround.Location = new System.Drawing.Point(0, 0);
+            this.playGround.Name = "playGround";
+            this.playGround.Size = new System.Drawing.Size(800, 450);
+            this.playGround.TabIndex = 0;
+            // 
+            // ball
+            // 
+            this.ball.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(3, 3);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // paddle
+            // 
+            this.paddle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.paddle.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.paddle.Location = new System.Drawing.Point(29, 3);
+            this.paddle.Name = "paddle";
+            this.paddle.Size = new System.Drawing.Size(200, 20);
+            this.paddle.TabIndex = 1;
+            this.paddle.TabStop = false;
+            // 
+            // UI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "ui";
+            this.Controls.Add(this.playGround);
+            this.Name = "UI";
             this.Text = "UI";
             this.Load += new System.EventHandler(this.Ui_Load);
+            this.playGround.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel playGround;
+        private System.Windows.Forms.PictureBox paddle;
+        private System.Windows.Forms.PictureBox ball;
     }
 }
 
